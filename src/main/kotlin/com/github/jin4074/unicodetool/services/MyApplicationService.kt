13291c36826875417559353be87605c4c4549b36
GitHub Action <action@github.com>
2020-08-08 13:59:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/jin4074/unicodetool/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/jin4074/unicodetool/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.jin4074.unicodetool.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jin4074.unicodetool.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/jin4074/unicodetool/services/MyProjectService.kt
+++ src/main/kotlin/com/github/jin4074/unicodetool/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.jin4074.unicodetool.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.jin4074.unicodetool.MyBundle
 
 class MyProjectService(project: Project) {
 
